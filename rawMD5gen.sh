def37b58a4847ab7ee80476826e6d59f8794cf06
ReshephTheGray <kingscradle@gmail.com>
2018-09-22 13:44:42 +0200

    Update rawMD5gen.sh
    Changed hashes to uppercase

--- rawMD5gen.sh
+++ rawMD5gen.sh
@@ -2,5 +2,5 @@
 # usage: ./rawMD5gen.sh <wordlist> < <new file>
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo -n $line | md5sum | cut -d" " -f1
+    echo -n $line | md5sum | cut -d" " -f1 | tr a-z A-Z
 done < "$1"
