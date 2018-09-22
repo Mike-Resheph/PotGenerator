56e9557638f876d2b467137541abc65c204a810d
ReshephTheGray <kingscradle@gmail.com>
2018-09-22 13:29:07 +0200

    Update rawMD5gen.sh
    Fixed an issue with trailing newlines

--- rawMD5gen.sh
+++ rawMD5gen.sh
@@ -2,5 +2,5 @@
 # usage: ./rawMD5gen.sh <wordlist> < <new file>
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo $line | md5sum | cut -d" " -f1
+    echo -n $line | md5sum | cut -d" " -f1
 done < "$1"
