d0f431f3612ab0d9635b02a85847815e60d34e86
ReshephTheGray <kingscradle@gmail.com>
2019-08-03 15:18:12 +0200

    Simplified and corrected hash format issue

--- NTLMv1gen.py
+++ NTLMv1gen.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # usage: ./NTLMv1gen.py <wordlist> > <new file>
 
-import hashlib,binascii,sys
+import hashlib,sys
 
 file = open(sys.argv[1])
 for line in file:
-	hash = hashlib.new('md4', line.encode('utf-16le')).digest();
-	print binascii.hexlify(hash)
+	try:
+		hash = hashlib.new('md4', line.encode('utf-16le')).hexdigest();
+#		print binascii.hexlify(hash)
+	except:
+		print ""
