b323568e3d00cf21e818dedf511ec4a45a11bb09
ReshephTheGray <kingscradle@gmail.com>
2019-08-03 15:19:30 +0200

    Fixed hashing issue

--- NTLMv1gen.py
+++ NTLMv1gen.py
@@ -7,6 +7,6 @@ file = open(sys.argv[1])
 for line in file:
 	try:
 		hash = hashlib.new('md4', line.encode('utf-16le')).hexdigest();
-#		print binascii.hexlify(hash)
+		print hash
 	except:
 		print ""
